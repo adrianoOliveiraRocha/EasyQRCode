cbbac5caf4983c6db6f6d36ef846d148be585111
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2020-11-23 13:41:35 -0300

    #

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,43 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import * as Linking from 'expo-linking';
-import {
-  AdMob,
-  AdMobBanner,
-  AdMobInterstitial,
-  PublisherBanner,
-  AdMobRewarded,
-  setTestDeviceIDAsync,
-} from 'expo-ads-admob';
+import { PublisherBanner} from 'expo-ads-admob';
 
+// test id ca-app-pub-3940256099942544/6300978111
+// real id pub-7854818002814670
 export default function Home({ navigation }) {
-
-  // React.useEffect(() => {
-  //   (async () => {
-  //
-  //     await AdMobRewarded.setAdUnitID('ca-app-pub-3940256099942544/5224354917');
-  //
-  //     await AdMobRewarded.requestAdAsync();
-  //
-  //     await AdMobRewarded.showAdAsync()
-  //       .then(result => {
-  //         console.log(result);
-  //       })
-  //       .catch(error => {
-  //         console.error("Error: " + error);
-  //       })
-  //   })();
-  //
-  // }, []);
-
-  async function showAdd() {
-    // pub-7854818002814670
-    await AdMobRewarded.setAdUnitID('ca-app-pub-3940256099942544/5224354917');
-
-    await AdMobRewarded.requestAdAsync();
-
-    await AdMobRewarded.showAdAsync()
-  }
 
   function openPrivacyPolicy() {
     Linking.openURL("https://sites.google.com/view/easy-qrcode/home");
@@ -54,9 +22,7 @@ export default function Home({ navigation }) {
 
       <View style={styles.headerContainer}>
         <Image style={styles.tinyLogo} source={require('./../../assets/icon.png')} />
-        {/*
-        <Text style={styles.subTitle}>Leitor de QRCode</Text>
-        */}
+
       </View>
 
       <View style={styles.bodyContainer}>
@@ -72,16 +38,11 @@ export default function Home({ navigation }) {
         />
       </View>
 
-      <View style={styles.adMobiContainer}>
-      <Button title="test" onPress={showAdd}/>
-
       <PublisherBanner
         bannerSize="fullBanner"
-        adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
+        adUnitID="pub-7854818002814670"
         onDidFailToReceiveAdWithError={bannerError}
-        onAdMobDispatchAppEvent={showAdd} />
-
-      </View>
+      />
 
       <StatusBar style="auto" />
 
@@ -101,18 +62,11 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around'
   },
   bodyContainer: {
-    flex: 0.4,
+    flex: 0.6,
     backgroundColor: '#4b85c5',
     alignItems: 'center',
     justifyContent: 'space-around',
     // padding: 15
-  },
-  adMobiContainer: {
-    flex: 0.2
-  },
-  bottomBanner: {
-    position: 'absolute',
-    bottom: 0,
   },
   tinyLogo: {
     width: 200,
